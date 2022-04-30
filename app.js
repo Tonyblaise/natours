1734f3cc03a026791c1e50710fe8c1f96061ddea
Tony Blaise <bubbablaise3@gmail.com>
2022-04-30 16:04:47 +0300

    Implementing a global erro handling middleware

--- app.js
+++ app.js
@@ -33,6 +33,36 @@ app.use('/api/v1/tours', userRouter)
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
 
+app.all('*', (req, res, next) => {
+//     res.status(404).json({
+//         status: 'fail',
+//         message:`Can't find ${req.originalUrl} on this server!`
+//     })
+    
+    const err = new Error(`Can't find ${req.originalUrl} on this server!`)
+    err.status = 'fail';
+    err.statusCode = 404;
+
+    next(err)
+ }
+    
+)
+
+//implemmenting global error handling middleware
+app.use((err, req, res, next) => {
+
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || "error";
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+    })
+
+
+})
+
+
 //Middlewares
 module.exports=app
 
--- server.js
+++ server.js
@@ -24,3 +24,4 @@ module.exports = app;
 
 //     res.send("You can post to this endpoint")
 // })
+//TEST
