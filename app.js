5430640e2b30393fc807928dc29ea1f21b272b44
Tony Blaise <bubbablaise3@gmail.com>
2022-05-11 11:00:56 +0300

    HTTP headers, data sanitization, parameter pollution

--- app.js
+++ app.js
@@ -5,13 +5,17 @@ const rateLimit=require('express-rate-limit')
 const AppError = require('./utils/appError')
 const app = express();
 const globalErrorHandler = require('./controllers/errorController')
+const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
+const hpp = require('hpp')
 
 //1 Middlewares
-
+//Development logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
-
+//limit requests from the same API
 const limiter = rateLimit({
     max: 100,
     windowMs: 60 * 60 * 1000,
@@ -19,9 +23,26 @@ const limiter = rateLimit({
     
 })
 
-app.use('/api',limiter)
+app.use('/api', limiter)
 
-app.use(express.json())
+//Set security HTTP headers
+app.use(helmet())
+
+//reading data from the body into req.body
+app.use(express.json({ limit: '10kb' }))
+
+//Data snitization against NOSQL query injection
+app.use(mongoSanitize())
+
+//Data sanitization against XSS
+app.use(xss())
+
+//prevent parameter pollution
+app.use(hpp({
+    whitelist:['duration','ratingsAverage','ratingsQuantity', 'maxGroupSize','difficulty','price']
+}))
+
+//serving static files
 app.use(express.static(`${__dirname}/public`))
 
 
